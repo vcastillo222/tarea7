af29b3409055fc53ee54304cd34ea77a0bb4ef9f
vcastillo222 <128198534+vcastillo222@users.noreply.github.com>
2025-04-15 18:27:11 -0400

    Update .cpp

--- .cpp
+++ .cpp
@@ -1,1 +1,15 @@
-
+Universidad Andrés Bello
+Facultad de Ingeniería
+Prof. J Köhler y M. Villanueva
+Tarea 7
+Análisis y Diseño de Algoritmos 1-2025
+Entrega: 17 de abril de 2025
+Objetivos
+Modelar un problema y resolverlo a través de un algoritmo. Evaluar la eficiencia del algoritmo.
+Actividad
+Desarrolle un algoritmo con retroceso (en castellano) para resolver el problema planteado a
+continuación. Determine su complejidad. Evalúe su algoritmo. Aplíquelo al caso planteado,
+detallando todos los pasos realizados. Analice y comente.
+Problema:
+A un mensajero se le solicita repartir paquetes a Machalí y a La Ligua. El peso de cada paquete y la
+ganancia obtenida por llevarlo a su destino se presenta en la tabla siguiente.
