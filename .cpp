c31d1b0a8a22fb3412151379cded116d2138d0bb
vcastillo222 <128198534+vcastillo222@users.noreply.github.com>
2025-04-16 10:49:00 -0400

    Update .cpp

--- .cpp
+++ .cpp
@@ -1,10 +1,4 @@
-Universidad Andrés Bello
-Facultad de Ingeniería
-Prof. J Köhler y M. Villanueva
-Tarea 7
-Análisis y Diseño de Algoritmos 1-2025
-Entrega: 17 de abril de 2025
-Objetivos
+/*
 Modelar un problema y resolverlo a través de un algoritmo. Evaluar la eficiencia del algoritmo.
 Actividad
 Desarrolle un algoritmo con retroceso (en castellano) para resolver el problema planteado a
@@ -13,3 +7,28 @@ detallando todos los pasos realizados. Analice y comente.
 Problema:
 A un mensajero se le solicita repartir paquetes a Machalí y a La Ligua. El peso de cada paquete y la
 ganancia obtenida por llevarlo a su destino se presenta en la tabla siguiente.
+
+
+*/
+
+#include <>
+
+#define 
+
+ // algoritmo con retroceso -> 
+//algoritmo visto en 551
+    class paquete()
+  {
+    int peso, ganancia;
+    string destino;
+    public:
+    //cada destino tiene un peso y una ganancia. 
+  }
+
+int main()
+{
+
+  
+  
+    return 0;
+}
